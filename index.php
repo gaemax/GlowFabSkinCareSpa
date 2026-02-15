ecb13c62d832b638a85ed72b5847013e97d7085c
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-15 21:55:09 +0800

    add basic html to index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo '<!DOCTYPE html>';
+    echo '<html>';
+
+    echo '<head>';
+    echo '<title>Glow Fab</title>';
+    echo '</head>';
+
+    echo '<body>';
+
+    echo '<h1>Hello World</h1>';
+
+    echo '</body>';
+
+    echo '</html>';
+
+?>
