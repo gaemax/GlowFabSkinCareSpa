b0b69005408fcc0b76ba8e0f3ce63d401b166864
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-17 23:42:29 +0800

    alter navigation bar code and style

--- index.php
+++ index.php
@@ -1,22 +1,30 @@
-
+<?php
+    session_start();
+    if(isset($_SESSION["loggedin"])) {
+        header("location: login");
+    }
+?>
 
 <html>
     <head>
-        <title>GLOWFABSKINCARESPA</title>
+        <title>Glow Fab</title>
         <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Poppins" />
     </head>
     <body>
         
-    <div class="header" width="">
-        <h1>Glow Fab</h1>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Services</li>
-            <li>Staff</li>
-            <li>Reviews</li>
-            <li>Contact Us</li>
-        </ul>
-    </div>
+    <header>
+        <div class="navigatorBar" width="">
+            <h1>Glow Fab</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Staff</li>
+                <li>Reviews</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+    </header>
     </body>
 </html>
